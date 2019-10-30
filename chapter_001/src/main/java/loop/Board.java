fdc59012f06a261a2f2c227e314acc20a4960365
Odilsho Karamalishoev <odilshoozirsho@gmail.com>
2019-10-30 13:47:37 +0300

    Board

---
+++ chapter_001/src/main/java/loop/Board.java
@@ -0,0 +1,24 @@
+package loop;
+
+public class Board {
+    public static void paint(int width, int height) {
+        for (int i=0;i<=width;i++ ) {
+            for ( int j=0;j<=width;j++) {
+
+                if ((i+j)%2==0) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            // добавляем перевод на новую строку.
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+    }
+}
