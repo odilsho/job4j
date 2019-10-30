600da6e744185119e7e422d628f2cdecc9d04b3f
Odilsho Karamalishoev <odilshoozirsho@gmail.com>
2019-10-30 13:03:51 +0300

    Counter

--- chapter_001/src/test/java/condition/Counter.java
+++ chapter_001/src/test/java/condition/Counter.java
@@ -4,7 +4,7 @@ public class Counter {
     public int add(int start, int finish) {
         int sum = 0;
         for (int i = 0; i <= finish; i++) {
-            sum+=i;
+            sum += i;
         }
         return sum;
     }
--- chapter_001/src/test/java/loop/CounterTest.java
+++ chapter_001/src/test/java/loop/CounterTest.java
@@ -1,8 +1,9 @@
 package loop;
+
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
 
 public class CounterTest {
     @Test
