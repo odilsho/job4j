c0a6015a1665d503e23616ca48f9d3fa87427e68
Odilsho Karamalishoev <odilshoozirsho@gmail.com>
2019-11-02 23:14:16 +0300

    "Array char"

---
+++ chapter_001/src/main/java/array/ArrayChar.java
@@ -0,0 +1,17 @@
+package array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = false;
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] == pref[i])
+                result = true;
+            else
+                result = false;
+        }
+
+
+        return result;
+    }
+}
