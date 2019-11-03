8adacdbced4a98a3936473831af48085b3010fe7
Odilsho Karamalishoev <odilshoozirsho@gmail.com>
2019-11-03 12:15:34 +0300

    "Find array min "

--- chapter_001/src/main/java/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/loop/CheckPrimeNumber.java
@@ -2,8 +2,8 @@ package loop;
     public class CheckPrimeNumber {
         public boolean check(int finish) {
             boolean prime = false;
-            for (int i = 0; i < finish; i++) {
-                if (i%2==0)
+            for (int i = 2; i < finish; i++) {
+                if (finish%2!=0)
                     prime=true;
                 else
                     prime=false;
