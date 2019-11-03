b34f210fd4ba27f65f4180afac5dc095263bd34c
Odilsho Karamalishoev <odilshoozirsho@gmail.com>
2019-11-03 12:07:37 +0300

    "FindLoopImproved"

---
+++ chapter_001/src/test/java/array/FindLoop2Test.java
@@ -0,0 +1,45 @@
+package array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoop2Test {
+
+    @Test
+    public void whenArrayHasLength5Then0() {
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = FindLoop2.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHasNotLength5ThenFalse() {
+        int[] input = new int[] {10, 5, 3};
+        int value = 5;
+        int result = FindLoop2.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop2.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenNotFind3() {
+        int[] input = new int[] {5, 2, 10, 5, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop2.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
