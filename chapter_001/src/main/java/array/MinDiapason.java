5a2ed72c21104f57136c262dd09913819a234434
Odilsho Karamalishoev <odilshoozirsho@gmail.com>
2019-11-03 12:26:53 +0300

    "FindLoop"

---
+++ chapter_001/src/main/java/array/MinDiapason.java
@@ -0,0 +1,12 @@
+package array;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int i = start; i < finish; i++) {
+            if (array[i] < min)
+                min = array[i];
+        }
+        return min;
+    }
+}
