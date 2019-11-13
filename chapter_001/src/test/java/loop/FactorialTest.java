b72f44603fc0527946bc7a97fdc5377c8664bf2a
Odilsho Karamalishoev <odilshoozirsho@gmail.com>
2019-11-13 22:08:21 +0300

    "Factorial"

--- chapter_001/src/test/java/loop/FactorialTest.java
+++ chapter_001/src/test/java/loop/FactorialTest.java
@@ -8,12 +8,13 @@ import static org.junit.Assert.assertThat;
 public class FactorialTest {
     @Test
     public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
-        int result=Factorial.calc(5);
-        assertThat(result,is(120));
+        int result = Factorial.calc(5);
+        assertThat(result, is(120));
     }
+
     @Test
     public void whenCalculateFactorialForZeroThenOne() {
-        int result=Factorial.calc(0);
-        assertThat(result,is(1));
+        int result = Factorial.calc(0);
+        assertThat(result, is(1));
     }
 }
