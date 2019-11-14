025a367aa5d4e8e1a4cc05364203a966aa9df44d
Odilsho Karamalishoev <odilshoozirsho@gmail.com>
2019-11-14 17:52:06 +0300

    "SqMax"

---
+++ chapter_001/src/main/java/condition/SqMax.java
@@ -0,0 +1,21 @@
+package condition;
+
+public class SqMax {
+    public static int max(int first, int second, int third, int forth) {
+        int result = forth;
+        if (first > second) {
+            if (first > third) {
+                if (first > forth) {
+                    result = first;
+                }
+            }
+        }  if (second > third) {
+            if (second > forth) {
+                result = first;
+            }
+        } else if (third > forth) {
+            result = second;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/condition/SqMaxTest.java
@@ -0,0 +1,27 @@
+package condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SqMaxTest {
+
+    @Test
+    public  void When4321Then4(){
+
+        int result=SqMax.max(4,3,2,1);
+        assertThat(result,is(4));
+
+
+    }
+    @Test
+    public  void When102304Then30(){
+
+        int result=SqMax.max(10,2,30,4);
+        assertThat(result,is(30));
+
+
+    }
+
+}
