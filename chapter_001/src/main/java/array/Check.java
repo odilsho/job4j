40e91feabce9ccde7654662197ae832f99695aa0
Odilsho Karamalishoev <odilshoozirsho@gmail.com>
2019-11-15 16:52:56 +0300

    "Check"

--- chapter_001/src/main/java/array/Check.java
+++ chapter_001/src/main/java/array/Check.java
@@ -2,12 +2,12 @@ package array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
+        boolean result = true;
         boolean etalon = data[0];
         for (int i = 0; i < data.length / 2; i++) {
-            if (etalon == data[i + 1])
-                result = true;
-            else result = false;
+            if (etalon != data[i + 1])
+                result = false;
+
         }
         return result;
     }
--- chapter_001/src/test/java/array/CheckTest.java
+++ chapter_001/src/test/java/array/CheckTest.java
@@ -3,6 +3,7 @@ package array;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+
 import static org.junit.Assert.assertThat;
 
 public class CheckTest {
